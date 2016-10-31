2c2728b8594e0b325b94096e92295aef67ebb89f
leo <the_guardian2@hotmail.com>
2016-10-31 09:55:52 +0100

    Rename English_admin.php to NEW_English_admin.php
    Disabled the UPLOAD since only url's can be added. As all files (mp3 m4a ogg) f.e in Chrome tested do work, a SINGLE field would be sufficient.. and help : pointing out types and browser warning). (Opera has no m4a support and IE no ogg) FF can take them all..

---
+++ languages/NEW_English_admin.php
@@ -0,0 +1,49 @@
+<?php
+
+// Always use the format LAN_PLUGIN_{FOLDER}_{TYPE} to prevent conflicts. In this case "_BLANK" is the folder. 
+// This should contain the LANs used in the plugin.xml file. 
+
+define("LAN_JPLAYER_ADMIN_07", "Playlist name");
+define("LAN_JPLAYER_ADMIN_08", "Logo (120px X 50px)");
+define("LAN_JPLAYER_ADMIN_11", "Defaut Artwork (80px X 80px)");
+define("LAN_JPLAYER_ADMIN_12", "Autoplay (not recommended)");
+define("LAN_JPLAYER_ADMIN_04", "Create a new playlist");    /* OK */ 
+
+
+define("LAN_JPLAYER_ADMIN_37", "Add a new song");
+define("LAN_JPLAYER_ADMIN_38", "Choose Playlist");
+define("LAN_JPLAYER_ADMIN_39", "Song title:");
+define("LAN_JPLAYER_ADMIN_40", "Artist:");
+define("LAN_JPLAYER_ADMIN_41", "General music file URL:");
+//define("LAN_JPLAYER_ADMIN_42", "(Enter an URL or choose a song from the media library)");
+//define("LAN_JPLAYER_ADMIN_43", "Choose an mp3 file");
+//define("LAN_JPLAYER_ADMIN_44", "Optional Ogg URL:");
+//define("LAN_JPLAYER_ADMIN_45", "(Optional ogg file for firefox and opera)");
+//define("LAN_JPLAYER_ADMIN_46", "Choose an ogg file");
+define("LAN_JPLAYER_ADMIN_47", "Artwork (jpg or png) (80px X 80px):");
+define("LAN_JPLAYER_ADMIN_48", "Itunes URL:");
+define("LAN_JPLAYER_ADMIN_49", "Amazon URL:");
+define("LAN_JPLAYER_ADMIN_50", "Other 'buy' URL:");
+define("LAN_JPLAYER_ADMIN_51", "Free download");
+define("LAN_JPLAYER_ADMIN_52", "Will overwrite the 'buy' URLs above");
+define("LAN_JPLAYER_ADMIN_53", "(MP3, M4a, OGG. Note : not all browsers support these formats !!)");
+
+
+define("LAN_JPLAYER_ADMMENU_01", "Manage playlists");   /* OK */
+define("LAN_JPLAYER_ADMMENU_02", "Settings");           /* OK */
+define("LAN_JPLAYER_ADMMENU_03", "Manage songs");   		/* OK */
+
+
+
+define("LAN_ADMIN_JPLAYER_OPTIONS_01", "Skin");
+define("LAN_ADMIN_JPLAYER_OPTIONS_02", "Background Color");
+define("LAN_ADMIN_JPLAYER_OPTIONS_03", "Background Opacity in percent");
+define("LAN_ADMIN_JPLAYER_OPTIONS_04", "Text and Icons Color");
+define("LAN_ADMIN_JPLAYER_OPTIONS_05", "Enable scrollbar");
+define("LAN_ADMIN_JPLAYER_OPTIONS_06", "Number of songs to display before showing a scrollbar");
+define("LAN_ADMIN_JPLAYER_OPTIONS_07", "Generate  facebook and google plus metadata on playlist single page");
+define("LAN_ADMIN_JPLAYER_OPTIONS_08", "Disable flash fallback (*)");
+define("LAN_ADMIN_JPLAYER_OPTIONS_09", "Disable flash on Android (**)");
+define("LAN_ADMIN_JPLAYER_OPTIONS_10", "Infos");
+define("LAN_ADMIN_JPLAYER_OPTIONS_11", "* If you choose to disable the flash fallback, you will have to upload an ogg file for each song. This is useful only if you care about the compatibility with firefox on IOS.");
+define("LAN_ADMIN_JPLAYER_OPTIONS_12", "Player Color Settings"); /* OK */
