eafdac2f4d1d60715301afa65c000431d18475e9
tgtje <the_guardian2@hotmail.com>
2016-11-10 00:31:21 +0100

    International translations possible; V2 standard on e107::lan

---
+++ languages/English/English_front.php
@@ -0,0 +1,20 @@
+<?php
+
+define("LAN_FRONT_01", "Buy on iTunes");
+define("LAN_FRONT_02", "Buy on amazon");
+define("LAN_FRONT_03", "Buy now");
+define("LAN_FRONT_04", "Right click and save link to download the mp3");
+define("LAN_FRONT_05", "by");
+define("LAN_FRONT_06", "Share on facebook");
+define("LAN_FRONT_07", "Share on twitter");
+define("LAN_FRONT_08", "close");
+define("LAN_FRONT_09", "menu");  
+define("LAN_FRONT_10", "shuffle");  /* */
+define("LAN_FRONT_11", "shuffle off");
+define("LAN_FRONT_12", "repeat");
+define("LAN_FRONT_13", "repeat off");
+define("LAN_FRONT_14", "share");    /* */
+define("LAN_FRONT_15", "This playlist does not exist or is empty. Please double check the playlist ID and be sure you have uploaded songs.");
+define("LAN_FRONT_16", "This playlist does not exist or is empty.");
+
+?>
